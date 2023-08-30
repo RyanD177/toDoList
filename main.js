2092f26c3e32eae072516e7971e4d4d90d462329
RyanD177 <w0477770@campus.nscc.ca>
2023-08-30 01:18:21 -0300

    js practice, creating toDo List

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
+const toDoList = document.getElementById('todo-list');
 const input = document.getElementById('todo-input');
-const todoList = document.getElementById('todo-list'); // Fix the ID here
 const addButton = document.getElementById('add-button');
+addButton.addEventListener('click', onClickAdd)
+input.addEventListener('input', onTypeToDo)
 
-addButton.addEventListener('click', onClickAdd);
-input.addEventListener('input', onTypeTodo);
 
-function onClickAdd() {
-  const li = createListItem(input.value);
-  todoList.appendChild(li);
-  input.value = "";
-  addButton.disabled = true;
+
+
+function onClickAdd () {
+    const li  = createListItem(input.value);
+    toDoList.appendChild(li);
+    input.value = "";
+    addButton.disabled = true;
 }
 
-function onTypeTodo() {
-  addButton.disabled = input.value.length === 0;
+function onTypeToDo () {
+    addButton.disabled = input.value.length === 0;
 }
 
-function createListItem(name) {
-  const li = document.createElement('li');
+function createListItem (name) {
+    const li = document.createElement('li');
+    const heading  = document.createElement('h2');
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = "X";
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', onClickDelete)
+    li.appendChild(heading);
+    li.appendChild(deleteButton)
+    heading.textContent = name;
+    return li
 
-  const heading = document.createElement('h2');
-  heading.textContent = name;
 
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'X';
-  deleteButton.classList.add('delete-button');
+}
 
-  li.appendChild(heading);
-  li.appendChild(deleteButton);
-
-  return li;
+function onClickDelete () {
+    this.parentNode.remove();
 }
